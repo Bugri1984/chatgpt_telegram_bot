9289beae18ec9e90b6b2f971d8b1423a9d477f73
Bugri1984 <ben469457@gmail.com>
2025-04-16 10:24:40 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,9 +8,6 @@ config_dir = Path(__file__).parent.parent.resolve() / "config"
 with open(config_dir / "config.yml", 'r') as f:
     config_yaml = yaml.safe_load(f)
 
-# load .env config
-config_env = dotenv.dotenv_values(config_dir / "config.env")
-
 # config parameters
 telegram_token = config_yaml["telegram_token"]
 openai_api_key = config_yaml["openai_api_key"]
@@ -21,10 +18,11 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = os.environ.get('MONGODB_URI')  # <-- читаем из переменной окружения
+
+# читаем переменную окружения от Render
+mongodb_uri = os.environ.get('MONGODB_URI')
 if mongodb_uri is None:
     raise ValueError("MONGODB_URI is not set in environment variables")
-
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
